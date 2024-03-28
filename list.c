b45fc4b66cbf61b7b0e073a00739838ede083b27
Your Name <you@example.com>
2024-03-28 05:18:52 +0000

    new try

--- list.c
+++ list.c
@@ -153,7 +153,7 @@ void pushCurrent(List * list, void * data)
   }
 
   nuevo->next = list->current->next;
-  if(list->current->next != NULL) list->current->next = nuevo;
+  if(list->current->next != NULL) list->current->next->prev = nuevo;
   nuevo->prev = list->current;
   list->current->next = nuevo;
   return;
